0a7edb7003c45e295e7e3681b673a2a348d2c914
Samantha Meason <sbmeason@gmail.com>
2020-02-16 19:48:42 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,7 +5,9 @@ class Anagram
     @word = word
   
   def match(array_of_words)
-    
+    array_of_words.select do |element|
+      (@word.split("").sort) == (element.split("").sort)
+    end
     
 end
 
